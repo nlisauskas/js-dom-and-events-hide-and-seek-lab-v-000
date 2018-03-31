a86ce263a8df14acf47b0a380140e49a9c84cc58
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-31 19:29:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementById('nested')
+  return document.querySelector('#nested .target')
 }
 
 function increaseRankBy(n) {
