c190fb1ccd01c18d053bda162b60a169792f8a52
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-03-31 19:28:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function getFirstSelector(selector) {
-  return document.querySelector('selector');
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested')
+}
+
+function increaseRankBy(n) {
+
+}
+
+function deepestChild() {
+
 }
